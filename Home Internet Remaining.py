1edff41055da670fc78d8e33b287d430f93893c9
AbdellrahmanAhmed <77490935+AbdellrahmanAhmed@users.noreply.github.com>
2022-06-10 05:10:43 +0200

    Update Home Internet Remaining.py
    1.0.2

--- Home Internet Remaining.py
+++ Home Internet Remaining.py
@@ -14,6 +14,15 @@ month = today.strftime("%B")
 time = now.strftime("%H:%M:%S")
 todayN = today.strftime("%A")
 
+
+def writeCell(cell0):
+    ws[f'C${cell0}'].value = todayN
+    ws[f'B${cell0}'].value = month
+    ws[f'A${cell0}'].value = today
+    ws[f'D${cell0}'].value = time
+    ws[f'E${cell0}'].value = remaining
+
+
 ws = wb.active
 cellValue = ws[f'A${cell}'].value
 
@@ -23,11 +32,7 @@ while cellValue:
     cell = cell + 1
     cellValue = ws[f'A${cell}'].value
 
-ws[f'A${cell}'].value = today
-ws[f'B${cell}'].value = month
-ws[f'C${cell}'].value = todayN
-ws[f'D${cell}'].value = time
-ws[f'E${cell}'].value = remaining
+writeCell(cell)
 
 cellList = ['A', 'B', 'C', 'D', 'E']
 
@@ -35,6 +40,21 @@ for cellC in cellList:
     currentCell = ws[f'${cellC}${cell}']
     currentCell.alignment = Alignment(horizontal='center')
 
-print('Last Remaining:  ', ws[f'E${cell - 1}'].value)
+# print(cell)
 
+lastRemaining = ws[f'E${cell - 1}'].value
+(used) = round(float(lastRemaining) - float(remaining), 2)
+# print(f'Last Used {round(used, 2)} GB')
+
+last_Remaining = ws[f'E${cell - 1}'].value
+
+tt = str(ws[f'D${cell - 1}'].value)
+
+bTime = datetime.strptime(tt, "%H:%M:%S")
+btTime = datetime.strptime(time, "%H:%M:%S") - bTime
+
+print(f'''
+Last Remaining: {last_Remaining}   Last time  {tt}
+Used {used} GB      Duration  {btTime}
+''')
 wb.save('Home Internet Remaining.xlsx')
